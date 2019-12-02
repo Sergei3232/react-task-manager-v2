db2a050d6f3bc39df17e04a08bca3e892a49f260
Sergei3232 <45172095+Sergei3232@users.noreply.github.com>
2019-12-02 13:42:44 +0300

    Добавление констант для экшенов

---
+++ src/constants/TODO.js
@@ -0,0 +1,15 @@
+export const GET_DATA_START = 'GET_DATA_START'
+export const GET_DATA_ERROR = 'GET_DATA_ERROR'
+export const GET_DATA_END   = 'GET_DATA_END'
+
+export const DELETE_START = 'DELETE_START'
+export const DELETE_ERROR = 'DELETE_ERROR'
+export const DELETE_END   = 'DELETE_END'
+
+export const EDIT_START = 'EDIT_START'
+export const EDIT_ERROR = 'EDIT_ERROR'
+export const EDIT_END   = 'EDIT_END'
+
+export const CREAT_START = 'CREAT_START'
+export const CREAT_ERROR = 'CREAT_ERROR'
+export const CREAT_END   = 'CREAT_END'
