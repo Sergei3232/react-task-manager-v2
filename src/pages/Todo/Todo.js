9802af5dacca85ea06387a2ca3e36ee5a4d7307e
Sergei3232 <45172095+Sergei3232@users.noreply.github.com>
2019-12-05 18:25:50 +0300

    Удаление хлама из центр страницы

--- src/pages/Todo/Todo.js
+++ src/pages/Todo/Todo.js
@@ -49,39 +49,6 @@ function Todo(props) {
             <div className="page-index__footer">
                 <p>© 2019 АО "Мегаполис"</p>
             </div>
-
-
-            {/* <div > */}
-            {/* <div className={css.h_title}>
-                        <h1>Список задач</h1>
-                    </div>
-                    <div className={css.h_adding}>
-                        <button className = {css.btn_icon}>
-                            <span>Добавить</span>
-                        </button>
-                    </div> */}
-
-            {/* </div> */}
-            {/* <div > */}
-            {/* {
-                    data.map((item, key) => {
-                        return <Row
-                            {...item}
-                            className={css.item}
-                            deleteRow={(id) => {deleteRow(id)}}
-                            
-                            editRow={(id) => {
-                                editRow({ id, text: "Редактирование" })
-                            }}
-                            key={key} />
-                    })
-                }   */}
-            {/* </div> */}
-            {/* <div > */}
-
-            {/* </div> */}
-
-
         </div>
     )
 }
